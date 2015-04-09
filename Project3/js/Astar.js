01554b2d2224cfdfd1b94cd25ac03e99d5545e1d
Wills Speegle <wspeegle@uga.edu>
2015-04-09 16:23:59 -0400

    project 3 showing all nodes, not all values

--- Project3/js/Astar.js
+++ Project3/js/Astar.js
@@ -235,7 +235,7 @@ function redraw()
 
             //ctx.drawImage(spritesheet, 4 * tileWidth, 0, tileWidth, tileHeight, Closed[i].x * tileWidth, Closed[i].y * tileHeight, tileWidth, tileHeight);
             ctx.fillStyle = "blue";
-        if(visited[i]!= null) {
+
             if (visited[i].x != pathStart[0] && visited[i].y != pathStart[1]) {
                 ctx.fillText(visited[i].g,
                     visited[i].x * tileWidth + 10,
@@ -260,18 +260,18 @@ function redraw()
                     visited[i].y * tileHeight + 15);
             } else {
                 //Fill in G H F values in all other squares
-                ctx.fillText(visited[i].g,
-                    visited[i].x * tileWidth + 10,
-                    visited[i].y * tileHeight + 55);
-                ctx.fillText(visited[i].h,
-                    visited[i].x * tileWidth + 45,
-                    visited[i].y * tileHeight + 55);
-                ctx.fillText(visited[i].f,
-                    visited[i].x * tileWidth + 10,
-                    visited[i].x * tileHeight + 15);
+                //ctx.fillText(visited[i].g,
+                   // visited[i].x * tileWidth + 10,
+                    //visited[i].y * tileHeight + 55);
+                //ctx.fillText(visited[i].h,
+                    //visited[i].x * tileWidth + 45,
+                    //visited[i].y * tileHeight + 55);
+                //ctx.fillText(visited[i].f,
+                    //visited[i].x * tileWidth + 10,
+                    //visited[i].x * tileHeight + 15);
 
             }
-        }
+
 
     }
     for (rp=0; rp<currentPath.length; rp++)
@@ -414,8 +414,14 @@ function pathing()
     //console.log(currentPath);
     redraw();
 }
+function dijksta()
+{
+    currentPath = [];
+    currentPath = findDij(world,pathStart,pathEnd);
+    redraw();
+}
 
-function findPath(world, pathStart, pathEnd)
+function findDij(world, pathStart, pathEnd)
 {
     console.log("finding path from start: " + pathStart[0] + "," + pathStart[1] + " to " +pathEnd[0] + "," +pathEnd[1]);
     // shortcuts for speed
@@ -653,6 +659,278 @@ function findPath(world, pathStart, pathEnd)
                         myPath.g = Math.round((myNode.g + distanceFunction(myNeighbors[i], myNode))*10) /10;
                         // estimated cost of entire guessed route to the destination
                        // myPath.f = myPath.g + distanceFunction(myNeighbors[i], mypathEnd);
+                        myPath.h = 0;
+                        myPath.f = Math.round((myPath.g + myPath.h)*10) /10;
+                        // remember this new path for testing above
+                        Open.push(myPath);
+                        visited.push(myPath);
+                        // mark this node in the world graph as visited
+                        AStar[myPath.value] = true;
+                        if (myPath.h == 0)
+                        {
+                            found = true;
+                        }
+
+                    }
+
+
+                }
+                // remember this route as having no more untested options
+                Closed.push(myNode);
+
+            }
+        } // keep iterating until the Open list is empty
+        console.log("exit calc");
+        return result;
+
+    }
+
+    // actually calculate the a-star path!
+    // this returns an array of coordinates
+    // that is empty if no path is possible
+    return calculatePath();
+
+} // end of findPath() function
+
+
+function findPath(world, pathStart, pathEnd)
+{
+    console.log("finding path from start: " + pathStart[0] + "," + pathStart[1] + " to " +pathEnd[0] + "," +pathEnd[1]);
+    // shortcuts for speed
+    var	abs = Math.abs;
+    var	max = Math.max;
+    var	pow = Math.pow;
+    var	sqrt = Math.sqrt;
+    var found = false;
+
+    // the world data are integers:
+    // anything higher than this number is considered blocked
+    // this is handy is you use numbered sprites, more than one
+    // of which is walkable road, grass, mud, etc
+    var maxWalkableTileNum = 0;
+
+    // keep track of the world dimensions
+    // Note that this A-star implementation expects the world array to be square:
+    // it must have equal height and width. If your game world is rectangular,
+    // just fill the array with dummy values to pad the empty space.
+    var worldWidth = world[0].length;
+    var worldHeight = world.length;
+    var worldSize =	worldWidth * worldHeight;
+
+    // which heuristic should we use?
+    // default: no diagonals (Manhattan)
+    //var distanceFunction = ManhattanDistance;
+    //var findNeighbors = function(){}; // empty
+
+
+
+    // alternate heuristics, depending on your game:
+
+    // diagonals allowed but no sqeezing through cracks:
+    // var distanceFunction = DiagonalDistance;
+    // var findNeighbors = DiagonalNeighbors;
+
+    // diagonals and squeezing through cracks allowed:
+    //var distanceFunction = DiagonalDistance;
+    //var findNeighbors = DiagonalNeighborsFree;
+
+    // euclidean but no squeezing through cracks:
+    var distanceFunction = EuclideanDistance;
+    var findNeighbors = DiagonalNeighbors;
+
+    // euclidean and squeezing through cracks allowed:
+    //var distanceFunction = EuclideanDistance;
+    // var findNeighbors = DiagonalNeighborsFree;
+
+
+
+    // distanceFunction functions
+    // these return how far away a point is to another
+
+    function ManhattanDistance(Point, Goal)
+    {	// linear movement - no diagonals - just cardinal directions (NSEW)
+        return  (abs(Point.x - Goal.x) + abs(Point.y - Goal.y));
+    }
+
+    function DiagonalDistance(Point, Goal)
+    {	// diagonal movement - assumes diag dist is 1, same as cardinals
+        return max(abs(Point.x - Goal.x), abs(Point.y - Goal.y));
+    }
+
+    function EuclideanDistance(Point, Goal)
+    {	// diagonals are considered a little farther than cardinal directions
+        // diagonal movement using Euclide (AC = sqrt(AB^2 + BC^2))
+        // where AB = x2 - x1 and BC = y2 - y1 and AC will be [x3, y3]
+        return Math.round((sqrt(pow(Point.x - Goal.x, 2) + pow(Point.y - Goal.y, 2))) *10) /10;
+    }
+
+    // Neighbors functions, used by findNeighbors function
+    // to locate adjacent available cells that aren't blocked
+
+    // Returns every available North, South, East or West
+    // cell that is empty. No diagonals,
+    // unless distanceFunction function is not Manhattan
+    function Neighbors(x, y)
+    {
+        var	N = y - 1,
+            S = y + 1,
+            E = x + 1,
+            W = x - 1,
+            myN = N > -1 && canWalkHere(x, N),
+            myS = S < worldHeight && canWalkHere(x, S),
+            myE = E < worldWidth && canWalkHere(E, y),
+            myW = W > -1 && canWalkHere(W, y),
+            result = [];
+        if(myN)
+            result.push({x:x, y:N});
+        if(myE)
+            result.push({x:E, y:y});
+        if(myS)
+            result.push({x:x, y:S});
+        if(myW)
+            result.push({x:W, y:y});
+        findNeighbors(myN, myS, myE, myW, N, S, E, W, result);
+        return result;
+    }
+
+    // returns every available North East, South East,
+    // South West or North West cell - no squeezing through
+    // "cracks" between two diagonals
+    function DiagonalNeighbors(myN, myS, myE, myW, N, S, E, W, result)
+    {
+        if(myN)
+        {
+            if(myE && canWalkHere(E, N))
+                result.push({x:E, y:N});
+            if(myW && canWalkHere(W, N))
+                result.push({x:W, y:N});
+        }
+        if(myS)
+        {
+            if(myE && canWalkHere(E, S))
+                result.push({x:E, y:S});
+            if(myW && canWalkHere(W, S))
+                result.push({x:W, y:S});
+        }
+    }
+
+
+    // returns boolean value (world cell is available and open)
+    function canWalkHere(x, y)
+    {
+        return ((world[x] != null) &&
+        (world[x][y] != null) &&
+        (world[x][y] != 1));
+    };
+
+    // Node function, returns a new object with Node properties
+    // Used in the calculatePath function to store route costs, etc.
+    function Node(Parent, Point)
+    {
+        var newNode = {
+            // pointer to another Node object
+            Parent:Parent,
+            // array index of this Node in the world linear array
+            value:Point.x + (Point.y * worldWidth),
+            // the location coordinates of this Node
+            x:Point.x,
+            y:Point.y,
+            // the heuristic estimated cost
+            // of an entire path using this node
+            f:0,
+            // the distanceFunction cost to get
+            // from the starting point to this node
+            g:0,
+            h: 0,
+            hasP: false
+        };
+
+        return newNode;
+    }
+
+    // Path function, executes AStar algorithm operations
+    function calculatePath()
+    {
+        console.log("enter calc path");
+        // create Nodes from the Start and End x,y coordinates
+        console.log("pathstart " +pathStart[0] +","+ pathStart[1] );
+        console.log("pathend " +pathEnd[0] + ","+ pathEnd[1] );
+
+
+        var	mypathStart = Node(null, {x:pathStart[0], y:pathStart[1]});
+        var mypathEnd = Node(null, {x:pathEnd[0], y:pathEnd[1]});
+        // create an array that will contain all world cells
+        var AStar = new Array(worldSize);
+        // list of currently open Nodes
+        Open = [mypathStart];
+        // list of closed Nodes
+        //var Closed = [];
+        // list of the final output array
+        var result = [];
+        // reference to a Node (that is nearby)
+        //var myNeighbors;
+        // reference to a Node (that we are considering now)
+        var myNode;
+        // reference to a Node (that starts a path in question)
+        var myPath;
+        // temp integer variables used in the calculations
+        var length, max, min, i, j;
+        // iterate through the open list until none are left
+        console.log("mypathEnd x,y" +mypathEnd.x+ ","+ mypathEnd.y);
+        Closed =[];
+        visited = [];
+        while(length = Open.length)
+        {
+            max = worldSize;
+            min = -1;
+            for(i = 0; i < length; i++)
+            {
+                if(Open[i].f < max)
+                {
+                    max = Open[i].f;
+                    min = i;
+                }
+            }
+            // grab the next node and remove it from Open array
+            myNode = Open.splice(min, 1)[0];
+            // is it the destination node?
+            //console.log("myNode value: " + myNode.value + " myPathEnd val: " + mypathEnd.value);
+            console.log("myNode x,y: " + myNode.x +","+myNode.y);
+            //console.log("mypathEnd x,y" +mypathEnd.x+ ","+ mypathEnd.y);
+            //if(myNode.value === mypathEnd.value)
+            if(myNode.x == mypathEnd.x && myNode.y == mypathEnd.y)
+            {
+                console.log("entering iffffff");
+                myPath = Closed[Closed.push(myNode) - 1];
+                do
+                {
+                    result.push([myPath.x, myPath.y, myPath.f, myPath.g, myPath.h]);
+                }
+                while (myPath = myPath.Parent);
+                // clear the working arrays
+                // AStar = Closed = Open = [];
+                // we want to return start to finish
+                result.reverse();
+            }
+            else if(!found)// not the destination
+            {
+
+                // find which nearby nodes are walkable
+                myNeighbors = Neighbors(myNode.x, myNode.y);
+
+                // test each one that hasn't been tried already
+                for(i = 0, j = myNeighbors.length; i < j; i++)
+                {
+                    //console.log("entering for #: " +i);
+                    myPath = Node(myNode, myNeighbors[i]);
+
+                    myPath.hasP = true;
+                    if (!AStar[myPath.value])
+                    {
+                        // estimated cost of this particular route so far
+                        myPath.g = Math.round((myNode.g + distanceFunction(myNeighbors[i], myNode))*10) /10;
+                        // estimated cost of entire guessed route to the destination
+                        // myPath.f = myPath.g + distanceFunction(myNeighbors[i], mypathEnd);
                         myPath.h = ManhattanDistance(myNeighbors[i], mypathEnd);
                         myPath.f = Math.round((myPath.g + myPath.h)*10) /10;
                         // remember this new path for testing above
